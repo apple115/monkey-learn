f9ec429cd9944fd75e7805fc763f3c604c2801d9
apple115 <2567031030@qq.com>
2025-02-03 21:40:50 +0800

    feat: 添加Call 和 Function 的支持

--- ast/ast.go
+++ ast/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"bytes"
 	"monkey/token"
+	"strings"
 )
 
 type Node interface {
@@ -143,6 +144,7 @@ type IntegerLiteral struct {
 	Value int64
 }
 
+func (il *IntegerLiteral) expressionNode() {}
 // TokenLiteral ...
 func (il *IntegerLiteral) TokenLiteral() string {
 	return il.Token.Literal
@@ -152,10 +154,6 @@ func (il *IntegerLiteral) String() string {
 	return il.Token.Literal
 }
 
-// name ...
-func (il *IntegerLiteral) expressionNode() {
-
-}
 
 type PrefixExpression struct {
 	Token    token.Token
@@ -254,5 +252,59 @@ func (ie *IfExpression) String() string {
 		out.WriteString("else ")
 		out.WriteString(ie.Alternative.String())
 	}
+	return out.String()
+}
+
+
+// fn(x, y) {
+//   return x + y;
+// }
+type FunctionLiteral struct{
+	Token token.Token // fn
+	Parameters []*Identifier
+	Body *BlockStatement
+}
+
+// expressionNode ...
+func (fl *FunctionLiteral) expressionNode()  {}
+func (fl *FunctionLiteral) TokenLiteral() string{return fl.Token.Literal}
+func (fl *FunctionLiteral) String() string{
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params,p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params,","))
+	out.WriteString(")")
+	out.WriteString(fl.Body.String())
+
+	return out.String()
+}
+
+type CallExpression struct{
+	Token token.Token
+	Function Expression
+	Arguments []Expression
+}
+
+func(ce *CallExpression) expressionNode(){}
+func(ce *CallExpression) TokenLiteral() string {return ce.Token.Literal}
+func(ce *CallExpression) String()string{
+	var out bytes.Buffer
+
+	args := []string{}
+	for _, a := range ce.Arguments{
+		args = append(args, a.String())
+	}
+
+	out.WriteString(ce.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args,", "))
+	out.WriteString(")")
+
 	return out.String()
 }
--- parser/parser.go
+++ parser/parser.go
@@ -51,6 +51,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.FALSE, p.parseBoolean)
 	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
 	p.registerPrefix(token.IF, p.parseIfExpression)
+	p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
 	p.registerInfix(token.MINUS, p.parseInfixExpression)
@@ -60,6 +61,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.NOT_EQ, p.parseInfixExpression)
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
+	p.registerInfix(token.LPAREN, p.parseCallExpression)
 	return p
 }
 
@@ -161,6 +163,7 @@ var precedences = map[token.TokenType]int{
 	token.MINUS:    SUM,
 	token.SLASH:    PRODUCT,
 	token.ASTERISK: PRODUCT,
+	token.LPAREN:   CALL,
 }
 
 // peekPrecedence ...
@@ -261,6 +264,85 @@ func (p *Parser) parseBlockStatement() *ast.BlockStatement {
 		p.nextToken()
 	}
 	return block
+}
+
+// parseFunctionParameters ...
+func (p *Parser) parseFunctionParameters() []*ast.Identifier {
+	identifiers := []*ast.Identifier{}
+
+	//()
+	if p.peekTokenIs(token.RPAREN) {
+		p.nextToken()
+		return identifiers
+	}
+
+	//(x)
+	p.nextToken()
+	ident := &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+	identifiers = append(identifiers, ident)
+
+	//(x,y)
+	for p.peekTokenIs(token.COMMA) {
+		p.nextToken()
+		p.nextToken()
+		ident := &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+		identifiers = append(identifiers, ident)
+	}
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return identifiers
+}
+
+// parseFunctionLiteral ...
+func (p *Parser) parseFunctionLiteral() ast.Expression {
+	lit := &ast.FunctionLiteral{Token: p.curToken}
+
+	if !p.expectPeek(token.LPAREN) {
+		return nil
+	}
+
+	lit.Parameters = p.parseFunctionParameters()
+
+	if !p.expectPeek(token.LBRACE) {
+		return nil
+	}
+
+	lit.Body = p.parseBlockStatement()
+	return lit
+}
+
+// parseCallExpression ...
+func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
+	exp := &ast.CallExpression{Token: p.curToken, Function: function}
+	exp.Arguments = p.parseCallArguments()
+	return exp
+}
+
+// parseCallArguments ...
+func (p *Parser) parseCallArguments() []ast.Expression {
+	args := []ast.Expression{}
+
+	if p.peekTokenIs(token.RPAREN) {
+		p.nextToken()
+		return args
+	}
+
+	p.nextToken()
+	args = append(args, p.parseExpression(LOWEST))
+
+	for p.peekTokenIs(token.COMMA) {
+		p.nextToken()
+		p.nextToken()
+		args = append(args, p.parseExpression(LOWEST))
+	}
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return args
 }
 
 // parsePrefixExpression ...
